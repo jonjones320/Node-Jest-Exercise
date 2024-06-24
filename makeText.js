8da2498f3805f7eb2117f40eed2b27569e4d6a3a
JJ <jonjones320@gmail.com>
2024-06-23 17:13:40 -0700

    Built out the Markov Machine and the script.

--- makeText.js
+++ makeText.js
@@ -1,2 +1,38 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require('fs');
+const axios = require('axios');
+const { MarkovMachine } = require('./markov');
+
+async function textFromFile(filePath) {
+    try {
+        let content = fs.readFileSync(filePath, 'utf-8');
+        let markMachine = new MarkovMachine(content);
+        console.log(markMachine.makeText());
+    } catch (error) {
+        console.error("Error reading file: ", error);
+        process.exit(1);
+    }
+}
+
+async function textFromURL(url) {
+    try {
+        let response = await axios.get(url);
+        let markMachine = newMarkovMachine(response.data);
+        console.log(markMachine.makeText());
+    } catch (err) {
+        console.error("Error retreiving URL: ", error);
+        process.exit(1);
+    }
+}
+
+let [method, path] = process.argv.slice(2);
+
+if (method === 'file') {
+    textFromFile(path);
+} else if (method === 'url') {
+    textFromURL(path);
+} else {
+    console.error("Unknown method. Try a file or url.");
+    process.exit(1);
+}
--- markov.js
+++ markov.js
@@ -17,13 +17,32 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    this.chains = {};
+
+    for(let i = 0; i < this.words.length; i++) {
+      let word = this.words[i];
+      let nextWord = this.words[i + 1] || null;
+      if (!this.chains[word]) {
+        this.chains[word] = [];
+      }
+      this.chains[word].push(nextWord)
+    }
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let keys = Object.keys(this.chains);
+    let key = keys[Math.floor(Math.random() * keys.length)];
+    let output = [];
+
+    while (output.length < numWords && key !== null) {
+      output.push(key);
+      let nextWords = this.chains[key];
+      key = nextWords[Math.floor(Math.random() * nextWords.length)];
+    }
+
+    return output.join(" ")
   }
 }
